c70f1dee88c0493561d50636c49058cd4b582983
henvezz95 <henvezz@googlemail.com>
2024-01-16 17:38:53 +0100

    Solved bug

--- algorithms/ultralytics_detector.py
+++ algorithms/ultralytics_detector.py
@@ -1,9 +1,29 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, RTDETR
 
 
 class YOLO_detector:
     def __init__(self, model_path, imgsz):
         self.model = YOLO(model_path, task='detect')
+        self.imgsz = imgsz
+
+    def detect(self, img):
+        detection = self.model(img.astype('uint8'), verbose=False, imgsz=self.imgsz)
+        names = detection[0].names
+        result = []
+        predictions = []
+        confidences = []
+        boxes = detection[0].boxes.data.cpu().numpy()
+        for box in (boxes):
+            x0, y0, x1, y1, conf, pred = box
+            result.append([x0,y0, x1-x0, y1-y0])
+            predictions.append(names[int(pred)])
+            confidences.append(conf)
+
+        return result, predictions, confidences #Boxes, classes, confidence scores
+    
+class RTDETR_detector:
+    def __init__(self, model_path, imgsz):
+        self.model = RTDETR(model_path)
         self.imgsz = imgsz
 
     def detect(self, img):
