c2025280db0dfb19f72171f87df9b03f26591ebc
Enrico Vezzali <46928281+Henvezz95@users.noreply.github.com>
2025-11-07 16:08:53 +0100

    tChanged network name

--- scripts/k_fold_training_bafalo.sh
+++ scripts/k_fold_training_bafalo.sh
@@ -6,5 +6,5 @@ do
     echo $i
     python3 python/generate_coco_annotations.py -c ./config/generate_coco_annotations_config.yaml  -k $i
     python3 python/convert_coco_to_yolo.py -c ./annotations/COCO/ -o "./dataset/" 
-    python3 BaFaLo/bafalo_trainer.py -c ./config/bafalo_training_config.yaml -o "./Saved Models/fastscnn_0.25x_192-448_${i}"
+    python3 BaFaLo/bafalo_trainer.py -c ./config/bafalo_training_config.yaml -o "./Saved Models/bafalo_scnn_0.25x_192-448_${i}"
 done
